6f4dcfdfe758efca4a7de2755286a1b439f1b140
Johnny Reina <johnny.r.reina@gmail.com>
2019-02-17 02:17:19 -0600

    Cleans up example Adds docs

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -1,6 +1,17 @@
 import Euler
 
+-- The ODE
 slope x y = x - y
 
+-- Initial values
+x_0 = 1.5
+y_0 = 2
+
+-- Step size
+h = 0.2
+
+-- Number of steps
+steps = 5
+
 main :: IO ()
-main = print $ euler slope 1.5 2 0.2 5
+main = print $ euler slope x_0 y_0 h steps
